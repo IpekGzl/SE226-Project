d2b36e3fa20c7a8f9c6e55c913979a2542006da8
IpekGzl <ipekguzel03@gmail.com>
2026-05-23 15:09:34 +0300

    Update deduplicator.py

--- utils/deduplicator.py
+++ utils/deduplicator.py
@@ -1,1 +1,22 @@
+class Deduplicator:
 
+    def remove_duplicate_tracks(self, tracks):
+
+        unique_tracks = []
+
+        seen = []
+
+        for track in tracks:
+
+            key = (
+                track.title.lower(),
+                track.artist.lower()
+            )
+
+            if key not in seen:
+
+                seen.append(key)
+
+                unique_tracks.append(track)
+
+        return unique_tracks
