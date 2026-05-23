bdd7c800bb7e91e28c61e077ea94f4bd4ff1b6f4
IpekGzl <ipekguzel03@gmail.com>
2026-05-23 15:09:14 +0300

    Update track_model.py

--- models/track_model.py
+++ models/track_model.py
@@ -1,1 +1,12 @@
 
+class TrackModel:
+
+    def __init__(self, title, artist, url):
+
+        self.title = title
+        self.artist = artist
+        self.url = url
+
+    def __str__(self):
+
+        return self.title + " - " + self.artist
