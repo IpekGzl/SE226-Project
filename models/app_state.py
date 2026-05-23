274b61837f08679a0789f8e9bd5a241dc4f80bbc
Eray <154343404+ozkaneray@users.noreply.github.com>
2026-05-24 01:50:05 +0300

    Add AppState class to manage application state

--- models/app_state.py
+++ models/app_state.py
@@ -1,1 +1,24 @@
+from typing import Optional
+from PIL import Image
 
+from models.album_model import AlbumModel
+from models.track_model import TrackModel
+
+
+class AppState:
+
+    def __init__(self) -> None:
+
+        self.album: Optional[AlbumModel] = None
+
+        self.tracks: list[TrackModel] = []
+
+        self.cover_image: Optional[Image.Image] = None
+
+    def reset(self) -> None:
+
+        self.album = None
+
+        self.tracks.clear()
+
+        self.cover_image = None
