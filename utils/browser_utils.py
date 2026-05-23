2028dacbcd6b51476c365eac5e149cdf357b6549
Eray <154343404+ozkaneray@users.noreply.github.com>
2026-05-24 01:25:14 +0300

    Add BrowserUtils class to open URLs in browser

--- utils/browser_utils.py
+++ utils/browser_utils.py
@@ -1,1 +1,12 @@
+import webbrowser
 
+
+class BrowserUtils:
+
+    @staticmethod
+    def open_track_url(url: str) -> None:
+        try:
+            webbrowser.open(url)
+
+        except Exception as e:
+            print(f"Browser open error: {e}")
